ec2ead523040b25331c3acf5d4f4f04e71aca39e
Justin French <justin@indent.com.au>
2009-10-28 17:20:29 +1100

    i don't seem to need these requires in rails/init.rb, maybe they're needed for gem plugins, guess i'll find out soon

--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,2 @@
 # coding: utf-8
-require File.join(File.dirname(__FILE__), *%w[.. lib formtastic semantic_form_builder])
-require File.join(File.dirname(__FILE__), *%w[.. lib formtastic semantic_form_helper])
 ActionView::Base.send :include, Formtastic::SemanticFormHelper
